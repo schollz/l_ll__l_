bb20b5679f17f2a40f658b801f28af51b003dc73
Zack Scholl <zack.scholl@gmail.com>
2022-08-29 07:21:50 -0700

    working

--- wash.lua
+++ wash.lua
@@ -18,6 +18,7 @@ function init()
   params:set("rev_return_level",6)
   params:set("rev_low_time",7)
   params:set("rev_mid_time",11)
+  params:add_number("timescale","timescale",1,1000,100)
   local notes={0,2,4,5,7,9,11}
   local octaves={0,12,24,36,48,60,72}
   local root=18
@@ -33,13 +34,14 @@ function init()
   for i,n in ipairs(note_ind) do
     note_pos[n]=math.floor(util.linlin(1,num_notes,1,127,i))
   end
-  for i=1,1 do
+  for i=1,10 do
     clock.run(function()
       while true do
         local note=notes[math.random(#notes)]+octaves[math.random(#octaves)]+root
-        local duration=math.random(500,2000)/100
-        print(note,duration)
-        engine.washmo(note,duration,0.5,0.5)
+        local duration=math.random(500,2000)/10000*params:get("timescale")
+        local attack=math.random(300,700)/1000
+        -- print(note,duration,attack)
+        engine.washmo(note,duration,attack,1-attack)
         clock.sleep(duration)
       end
     end)
@@ -72,25 +74,26 @@ function cleanup()
   for k,v in pairs(rev_params) do
     params:set(k,v)
   end
+
 end
 
+blend_mode=8
 function redraw()
   screen.clear()
+  screen.aa(0)
+  screen.blend_mode(0)
+  
   for n,v in ipairs(note_env) do
     if v>0.002 then
       local level=util.round(util.linexp(0.002,1,0.001,15,v))
+      local lw=n%2==1 and 1 or 2 -- util.round(util.linexp(0,1,1,2,v))
+      screen.line_width(math.floor(lw))
       screen.level(level)
       screen.move(note_pos[n],0)
       screen.line(note_pos[n],64)
       screen.stroke()
     end
   end
-  -- local r=util.linlin(0,0.3,1,60,amplitude)
-  -- screen.level(level)
-  -- screen.circle(64,32,r)
-  -- screen.fill()
-  -- screen.level(level+3)
-  -- screen.circle(64,32,r)
-  -- screen.stroke()
+
   screen.update()
 end
