52a6664f4146adc2cde09ff0358d401181e73d3a
Zack <zack.scholl@gmail.com>
2022-08-29 14:30:18 -0700

    refactoring

--- emission-spectrum.lua
+++ emission-spectrum.lua
@@ -1,56 +1,50 @@
 -- wash
 
+hs = include('lib/halfsecond')
+MusicUtil = require "musicutil"
 engine.name="EmissionSpectrum"
+
+max_note_num=14*4
 
 function init()
   initialize_params()
-  local notes={0,2,4,5,7,9,11}
-  local octaves={0,12,24,36,48,60,72}
-  local root=18
-  local note_ind={}
-  for _,n in ipairs(notes) do
-    for _,o in ipairs(octaves) do
-      table.insert(note_ind,n+o)
+
+  -- TODO: setup halfsecond
+
+  for sector=1,4 do
+    for i=1,2 do
+      clock.run(function()
+        while true do
+          local note_ind=notes[math.random(#notes)]+octaves[math.random(#octaves)]+root
+          local duration=math.random(500,2000)/10000*params:get("timescale")
+          local attack=math.random(300,700)/1000
+          -- print(note,duration,attack)
+          engine.washmo(note+root,duration,attack,1-attack)
+          clock.sleep(duration)
+        end
+      end)
     end
   end
-  table.sort(note_ind)
-  num_notes=#note_ind
-  note_pos={}
-  for i,n in ipairs(note_ind) do
-    note_pos[n]=math.floor(util.linlin(1,num_notes,1,127,i))
-  end
-  for i=1,10 do
-    clock.run(function()
-      while true do
-        local note=notes[math.random(#notes)]+octaves[math.random(#octaves)]+root
-        local duration=math.random(500,2000)/10000*params:get("timescale")
-        local attack=math.random(300,700)/1000
-        -- print(note,duration,attack)
-        engine.washmo(note+root,duration,attack,1-attack)
-        clock.sleep(duration)
-      end
-    end)
-  end
+
   clock.run(function()
     while true do
       clock.sleep(1/10)
       redraw()
     end
   end)
-  -- clock.run(function()
-  --   while true do
-  --     clock.sync(1)
-  --     engine.kick(40,6,0.05,1,1,0.3,0.8,0.15,0)
-  --   end
-  -- end)
+
   note_env={}
   for i=1,127 do
     table.insert(note_env,0)
   end
 end
 
+function build_scale()
+  notes = MusicUtil.generate_scale_of_length(params:get("root_note"), params:get("scale_mode"), 56)
+end
 
 function initialize_params()
+  -- setup reverb parameters to be overwritten
   rev_params={
     reverb=2,
     rev_eng_input=0,
@@ -66,7 +60,21 @@ function initialize_params()
   params:set("rev_return_level",6)
   params:set("rev_low_time",7)
   params:set("rev_mid_time",11)
-  params:add_number("timescale","timescale",1,1000,100)
+  
+  -- setup scales
+  scale_names={}
+  for i = 1, #MusicUtil.SCALES do
+    table.insert(scale_names, string.lower(MusicUtil.SCALES[i].name))
+  end
+  params:add{type = "option", id = "scale_mode", name = "scale mode",
+    options = scale_names, default = 5,
+    action = function() build_scale() end}
+  params:add{type = "number", id = "root_note", name = "root note",
+    min = 0, max = 127, default = 18, formatter = function(param) return MusicUtil.note_num_to_name(param:get(), true) end,
+    action = function() build_scale() end}
+
+
+  -- setup other parameters
   local params_menu={
     {id="timescale",name="timescale",min=0.01,max=10,exp=false,div=0.01,default=1},
   }  
@@ -84,8 +92,8 @@ function initialize_params()
 
   for i=1,4 do 
     local params_menu={
-      {id="start",name="start",min=1,max=num_notes,exp=false,div=1,default=(i-1)*25+1},
-      {id="end",name="end",min=1,max=num_notes,exp=false,div=1,default=i*25},
+      {id="start",name="start",min=1,max=num_notes,exp=false,div=1,default=(i-1)*max_note_num/4+1},
+      {id="end",name="end",min=1,max=num_notes,exp=false,div=1,default=i*max_note_num/4},
       {id="attack mean",name="attack mean",min=0.01,max=30,exp=true,div=0.01,default=10,formatter=function(param) return param:get().." s" end},
       {id="attack std",name="attack std",min=0.01,max=30,exp=true,div=0.01,default=3,formatter=function(param) return "+/-"..param:get().." s" end},
       {id="decay mean",name="decay mean",min=0.01,max=30,exp=true,div=0.01,default=10,formatter=function(param) return param:get().." s" end},
